586e83039d82ce18bde852a5e930d0090e5afcaa
Bryan Hunter <bdhunter3141@gmail.com>
2017-09-12 16:09:03 +0000

    Completed test site using EJS.

--- server.js
+++ server.js
@@ -7,7 +7,16 @@ app.set("view engine", "ejs");
 
 //index page
 app.get("/", (req, res) => {
-  res.render("pages/index");
+  const drinks = [
+    { name: "Bloody Mary", drunkness: 3 },
+    { name: "Martini", drunkness: 5 },
+    { name: "Scotch", drunkness: 10 }
+  ];
+  let tagline = "Any code of your own that you haven't looked at for six or more months might as well have been written by someone else.";
+  res.render("pages/index", {
+    drinks: drinks,
+    tagline: tagline
+  });
 });
 
 //about page
